626e0b47478b1c19a0e5c0bf3e3771294bf43d63
kazacube-msabri <msabri@kazacube.com>
2023-01-24 12:05:02 +0100

    [IMP] création du nouveau module, gestion des parcs

---
+++ parc_training/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "formation gestion des parc",
+
+    'summary': """
+        module developper au cours de la formation technique""",
+
+    'description': """
+        module developper au cours de la formation technique
+    """,
+
+    'author': "SABRI Mohamaed",
+    'website': "http://www.kazacube.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+
+
+    ],
+    # only loaded in demonstration mode
+}
